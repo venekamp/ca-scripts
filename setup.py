1c4c2a870878d7773ad6529bc8215ed4d20f189e
Gerben Venekamp <venekamp@gmail.com>
2017-07-11 16:37:41 +0200

    Add more annotations to setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,18 @@ from setuptools import setup
 
 setup(
     name='ca',
-    version='0.0.1',
+    version='0.1.0',
+    description='CA commandline utility',
+    author='Gerben Venekamp',
+    author_email='venekamp@gmail.com',
+    url='https://github.com/venekamp/ca-scripts/',
     py_modules=['ca'],
     install_requires=[
         'Click',
+        'Jinja2',
     ],
     entry_points='''
-        [console_scripts]
-        ca=ca:ca
+    [console_scripts]
+    ca=ca:ca
     ''',
 )
